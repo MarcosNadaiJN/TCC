efdd332e1b7a7d40ca6fc5b7e44da3ccd104ac26
Marcos Nadai Junior <junior_nadai@hotmail.com>
2021-08-19 07:27:25 -0300

    Cleaned unused code, Add onResume

--- app/src/main/java/com/app/tccv3/MainActivity.java
+++ app/src/main/java/com/app/tccv3/MainActivity.java
@@ -21,72 +21,42 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected final BetterActivityResult<Intent, ActivityResult> activityLaucher = BetterActivityResult.registerActivityForResult(this);
-
-    private FloatingActionButton newlist;
-
     private int TotalBooks;
     private int FinishedBooks;
     private int LeftBooks;
     private int TotalPages;
     private int ReadPages;
     private int LeftPages;
-    private ArrayList<String> IDBookLists;
 
-    private String teste;
-
+    private FloatingActionButton newlist;
+    BookListDAO DAO = new BookListDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BookListDAO DAO = new BookListDAO();
+        newlist = (FloatingActionButton) findViewById(R.id.floatingActionButton_newList);
+        newlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openNewListActivity();}
+        });
 
-        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                        }
-                    }
-                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         ListView BookListView = findViewById(R.id.listview_listofbooks);
         BookListView.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 DAO.allLists()));
-
-
-
-        newlist = (FloatingActionButton) findViewById(R.id.floatingActionButton_newList);
-        newlist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {openSomeActivityForResult();}
-        });
-
     }
-
 
     public void openNewListActivity() {
         Intent intent = new Intent(this, newListActivity.class);
         startActivity(intent);
-    }
-
-    public void openSomeActivityForResult(){
-        Intent intent = new Intent(this, newListActivity.class);
-        activityLaucher.launch(intent, result -> {
-            if (result.getResultCode() == Activity.RESULT_OK) {
-                Intent data = result.getData();
-            }
-        });
-    }
-
-
-    public void addIDBookLists(String IDBookLists) {
-        this.IDBookLists.add(IDBookLists);
     }
 }
