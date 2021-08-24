23558e354a34fdfbb7efd10ed4e4106504ad8fb9
Marcos Nadai Junior <junior_nadai@hotmail.com>
2021-08-24 08:24:06 -0300

    Add ImagePicker, rename Var's

--- app/src/main/java/com/app/tccv3/newBookActivity.java
+++ app/src/main/java/com/app/tccv3/newBookActivity.java
@@ -1,58 +1,74 @@
 package com.app.tccv3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
 
 public class newBookActivity extends AppCompatActivity {
 
+    ImageView BookImage;
     EditText BookName;
     EditText BookAuthor;
     TextView BookCategory;
     TextView BookTotalPages;
     EditText BookTotalPages_Value;
-    TextView CurrentPage_Book;
-    EditText CurrentPage_Book_Value;
-    TextView ReadPages_Book;
-    TextView ReadPages_Book_Value;
-    TextView LeftPages_Book;
-    TextView LeftPages_Book_Value;
+    TextView Book_CurrentPage;
+    EditText Book_CurrentPage_Value;
+    TextView Book_ReadPages;
+    TextView Book_ReadPages_Value;
+    TextView Book_LeftPages;
+    TextView Book_LeftPages_Value;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_book);
-
         InitializingFields();
-
         BookDAO DAO = new BookDAO();
-
         ConfigureCancelButton();
-
         ConfigureAddButton(DAO,BookName);
+        ImagePicker();
+    }
 
+    private void ImagePicker() {
+        BookImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.Companion.with(newBookActivity.this)
+                        //.galleryOnly()
+                        .crop()
+                        .start();
+            }
+        });
     }
 
     private void InitializingFields() {
+        BookImage = (ImageView) findViewById(R.id.BookImage);
         BookName = (EditText) findViewById(R.id.edittext_title_book);
         BookAuthor = (EditText) findViewById(R.id.edittext_author_book);
         BookCategory = (TextView) findViewById(R.id.textview_book_category);
         BookTotalPages = (TextView) findViewById(R.id.textview_total_pages_book);
         BookTotalPages_Value = (EditText) findViewById(R.id.edittext_total_pages_book_value);
-        CurrentPage_Book = (TextView) findViewById(R.id.textview_currentpage_book);
-        CurrentPage_Book_Value = (EditText) findViewById(R.id.edittext_currentpage_book_value);
-        ReadPages_Book = (TextView) findViewById(R.id.textview_readpages_book);
-        ReadPages_Book_Value = (TextView) findViewById(R.id.textview_readpages_book_value);
-        LeftPages_Book = (TextView) findViewById(R.id.textview_leftpages_book);
-        LeftPages_Book_Value = (TextView) findViewById(R.id.textview_leftpages_book_value);
+        Book_CurrentPage = (TextView) findViewById(R.id.textview_currentpage_book);
+        Book_CurrentPage_Value = (EditText) findViewById(R.id.edittext_currentpage_book_value);
+        Book_ReadPages = (TextView) findViewById(R.id.textview_readpages_book);
+        Book_ReadPages_Value = (TextView) findViewById(R.id.textview_readpages_book_value);
+        Book_LeftPages = (TextView) findViewById(R.id.textview_leftpages_book);
+        Book_LeftPages_Value = (TextView) findViewById(R.id.textview_leftpages_book_value);
     }
 
     private void ConfigureCancelButton() {
-        Button cancel = (Button) findViewById(R.id.button_add_book);
+        Button cancel = (Button) findViewById(R.id.button_cancel_book);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,26 +78,36 @@ public class newBookActivity extends AppCompatActivity {
     }
 
     private void ConfigureAddButton(BookDAO DAO, EditText BookName) {
-        Button add = (Button) findViewById(R.id.button_cancel_book);
+        Button add = (Button) findViewById(R.id.button_add_book);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                book newBook = createBook(BookName);
+                Book newBook = createBook(BookName);
                 save(newBook, DAO);
+
 
                 finish();
             }
         });
     }
 
-    private void save(book List, BookDAO DAO) {
+    private void save(Book List, BookDAO DAO) {
         DAO.save(List);
     }
 
     @NonNull
-    private book createBook(EditText BookName) {
+    private Book createBook(EditText BookName) {
         String bookName = BookName.getText().toString();
-        book newBook = new book(bookName);
+        Book newBook = new Book(bookName);
         return newBook;
+    }
+
+    //Sets PickedImage as BookImage
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri uri = data.getData();
+        BookImage.setImageURI(uri);
     }
 }
