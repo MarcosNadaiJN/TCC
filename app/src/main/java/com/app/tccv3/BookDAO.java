a2c8d259e70f7aec69a29033ce0b90868c5e256e
Marcos Nadai Junior <junior_nadai@hotmail.com>
2021-08-25 08:56:36 -0300

    Add Edit method

--- app/src/main/java/com/app/tccv3/BookDAO.java
+++ app/src/main/java/com/app/tccv3/BookDAO.java
@@ -7,9 +7,27 @@ public class BookDAO {
 
     private final static List<Book> BookList = new ArrayList<>();
 
+    private static int IDCounter = 1;
+
 
     public void save(Book book) {
+        Book.setID(IDCounter);
         BookList.add(book);
+        IDCounter++;
+    }
+
+    public void edit(Book book) {
+        Book foundedBook = null;
+        for (Book a :
+                BookList) {
+            if (a.getID() == book.getID()) {
+                foundedBook = a;
+            }
+        }
+        if (foundedBook != null) {
+            int BookPosition = BookList.indexOf(foundedBook);
+            BookList.set(BookPosition, book);
+        }
     }
 
     public List<Book> AllBooks() {
