710814f982ac2d22193d692d7a9a6537798f70e9
Marcos Nadai Junior <junior_nadai@hotmail.com>
2021-08-25 08:56:13 -0300

    Created setters

--- app/src/main/java/com/app/tccv3/Book.java
+++ app/src/main/java/com/app/tccv3/Book.java
@@ -7,13 +7,17 @@ import java.io.Serializable;
 
 public class Book implements Serializable {
 
-    Book(String name){
+    private static int ID;
+
+    Book(String name, String Author, String BookCategory, int totalPages, int CurrentPage,
+         int leftPages){
         //this.IDBook = UUID.randomUUID().toString();
         this.name = name;
         this.finished = false;
     }
 
     //private String IDBook;
+    private int BookID = 0;
     private Uri BookImage;
     private String name;
     private String Author;
@@ -23,6 +27,11 @@ public class Book implements Serializable {
     private int readPages;
     private int leftPages;
     private boolean finished;
+
+
+    public static int getID() {
+        return ID;
+    }
 
     public Uri getBookImage() {
         return BookImage;
@@ -54,6 +63,51 @@ public class Book implements Serializable {
 
     public int getLeftPages() {
         return leftPages;
+    }
+
+
+    public static void setID(int id) {
+        Book.ID = id;
+    }
+
+    public void setBookID(int bookID) {
+        BookID = bookID;
+    }
+
+    public void setBookImage(Uri bookImage) {
+        BookImage = bookImage;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public void setBookCategory(String bookCategory) {
+        BookCategory = bookCategory;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public void setReadPages(int readPages) {
+        this.readPages = readPages;
+    }
+
+    public void setLeftPages(int leftPages) {
+        this.leftPages = leftPages;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
     }
 
     @Override
