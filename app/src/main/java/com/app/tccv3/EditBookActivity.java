c17f66405885e7972c5b97f2e732069a1c51745c
Marcos Nadai Junior <junior_nadai@hotmail.com>
2021-08-26 08:49:49 -0300

    Removed unused fields, add "Finished" field

--- app/src/main/java/com/app/tccv3/EditBookActivity.java
+++ app/src/main/java/com/app/tccv3/EditBookActivity.java
@@ -19,15 +19,13 @@ public class EditBookActivity extends AppCompatActivity {
     ImageView Book_Image;
     EditText Book_Name;
     EditText Book_Author;
-    TextView Book_Category;
     TextView Book_TotalPages;
     EditText Book_TotalPages_Value;
     TextView Book_CurrentPage;
     EditText Book_CurrentPage_Value;
-    TextView Book_ReadPages;
-    TextView Book_ReadPages_Value;
     TextView Book_LeftPages;
     TextView Book_LeftPages_Value;
+    TextView Book_is_finished_value;
 
     Book book;
 
@@ -45,11 +43,11 @@ public class EditBookActivity extends AppCompatActivity {
         Book_Image.setImageURI(book.getBookImage());
         Book_Name.setText(book.getName());
         Book_Author.setText(book.getAuthor());
-        //BookCategory.setText(book.getBookCategory());
         Book_TotalPages_Value.setText(String.valueOf(book.getTotalPages()));
         Book_CurrentPage_Value.setText(String.valueOf(book.getCurrentPage()));
-        Book_ReadPages_Value.setText(String.valueOf(book.getReadPages()));
         Book_LeftPages_Value.setText(String.valueOf(book.getLeftPages()));
+        if (book.getFinished() == false) Book_is_finished_value.setText("Não");
+        else Book_is_finished_value.setText("Sim");
 
     }
 
@@ -69,15 +67,13 @@ public class EditBookActivity extends AppCompatActivity {
         Book_Image = (ImageView) findViewById(R.id.BookImage_editor);
         Book_Name = (EditText) findViewById(R.id.edittext_title_book_editor);
         Book_Author = (EditText) findViewById(R.id.edittext_author_book_editor);
-        Book_Category = (TextView) findViewById(R.id.textview_book_category_editor);
         Book_TotalPages = (TextView) findViewById(R.id.textview_total_pages_book_editor);
         Book_TotalPages_Value = (EditText) findViewById(R.id.edittext_total_pages_book_value_editor);
         Book_CurrentPage = (TextView) findViewById(R.id.textview_currentpage_book_editor);
         Book_CurrentPage_Value = (EditText) findViewById(R.id.edittext_currentpage_book_value_editor);
-        Book_ReadPages = (TextView) findViewById(R.id.textview_readpages_book_editor);
-        Book_ReadPages_Value = (TextView) findViewById(R.id.textview_readpages_book_value_editor);
         Book_LeftPages = (TextView) findViewById(R.id.textview_leftpages_book_editor);
         Book_LeftPages_Value = (TextView) findViewById(R.id.textview_leftpages_book_value_editor);
+        Book_is_finished_value = (TextView) findViewById(R.id.textView_book_is_finished_value);
     }
 
     private void ConfigureCancelButton() {
@@ -115,23 +111,19 @@ public class EditBookActivity extends AppCompatActivity {
 
         String bookName = Book_Name.getText().toString();
         String bookAuthor = Book_Author.getText().toString();
-        String bookCategory = Book_Category.getText().toString();
         String bookTotalPages = Book_TotalPages_Value.getText().toString();
         int book_TotalPages_value = Integer.parseInt(bookTotalPages);
         String bookCurrentPages = Book_CurrentPage_Value.getText().toString();
         int book_CurrentPages_value = Integer.parseInt(bookCurrentPages);
-        String book_ReadPages = Book_ReadPages_Value.getText().toString();
-        int book_ReadPages_value = Integer.parseInt(book_ReadPages);
-        String book_LeftPages = Book_LeftPages_Value.getText().toString();
-        int book_LeftPages_value = Integer.parseInt(book_LeftPages);
+        int book_LeftPages_value = book_TotalPages_value - book_CurrentPages_value;
 
         book.setName(bookName);
         book.setAuthor(bookAuthor);
-        book.setBookCategory(bookCategory);
         book.setTotalPages(book_TotalPages_value);
         book.setCurrentPage(book_CurrentPages_value);
-        book.setReadPages(book_ReadPages_value);
         book.setLeftPages(book_LeftPages_value);
+        book.setLeftPages(book_LeftPages_value);
+        book.setFinished(book_TotalPages_value == book_CurrentPages_value);
     }
 
     //Sets PickedImage as BookImage
