055e1c310d2292f7a5c83c88396a36c6d27a9eba
Marcos Nadai Junior <junior_nadai@hotmail.com>
2021-11-01 06:43:56 -0300

    Add ConfigureHeader

--- app/src/main/java/com/app/tccv3/MainActivity.java
+++ app/src/main/java/com/app/tccv3/MainActivity.java
@@ -4,25 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int TotalBooks;
-    private int TotalPages;
-    private int ReadPages;
-    private int LeftPages;
-    private int FinishedBooks;
-    private int LeftBooks;
+    TextView TotalBooks;
+    private int TotalBooks_value;
+    TextView TotalPages;
+    private int TotalPages_value;
+    TextView ReadPages;
+    private int ReadPages_value;
+    TextView LeftPages;
+    private int LeftPages_value;
+    TextView FinishedBooks;
+    private int FinishedBooks_value;
+    TextView LeftBooks;
+    private int LeftBooks_value;
 
     private FloatingActionButton newBook;
 
@@ -38,13 +43,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //----------------
-        TotalBooks = DAO.AllBooks().size();
-
-        //----------------
+        ConfigNewBookButton();
 
 
+        TotalBooks = findViewById(R.id.textview_totalbooksValue_booklistscreen);
+        TotalPages = findViewById(R.id.textView_totalpagesvalue_booklistscreen);
+        ReadPages = findViewById(R.id.textView_readpagesvalue_booklistscreen);
+        LeftPages = findViewById(R.id.textView_leaftpagesvalue_booklistscreen);
+        FinishedBooks = findViewById(R.id.textview_totalbooksValue2_booklistscreen);
+        LeftBooks = findViewById(R.id.textview_totalbooksValue3_booklistscreen);
+
+
+    }
+
+    private void ConfigNewBookButton() {
         newBook = findViewById(R.id.floatingActionButton_newBook);
         newBook.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
                 openNewBookActivity();
             }
         });
-
     }
 
     @Override
@@ -60,17 +71,26 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         ConfigureBookList();
 
+        ConfigureHeader();
 
-        int ListSize = DAO.AllBooks().size();
-        BookList = DAO.AllBooks();
-        if(ListSize > 0){
-            while (i < ListSize) {
-                CurrentBook = BookList.get(i);
-                TotalPages += CurrentBook.getTotalPages();
-                Log.i("TAG", "onResume: " + TotalPages);
-                i++;
-            }
-        }
+    }
+
+    private void ConfigureHeader() {
+
+        List<Integer> Values = DAO.infoHeader();
+
+        TotalBooks_value = Values.get(0);
+        TotalBooks.setText(Integer.toString(TotalBooks_value));
+
+        TotalPages_value = Values.get(1);
+        TotalPages.setText(Integer.toString(TotalPages_value));
+
+        ReadPages_value  = Values.get(2);
+        ReadPages.setText(Integer.toString(ReadPages_value));
+
+        LeftPages_value = TotalPages_value - ReadPages_value;
+        LeftPages.setText(Integer.toString(LeftPages_value));
+
 
     }
 
